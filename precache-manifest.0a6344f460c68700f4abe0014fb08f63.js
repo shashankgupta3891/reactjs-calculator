431d9947604bee4c93f1cf1c5a979ecc5182e474
shashank gupta <43991133+shashankgupta3891@users.noreply.github.com>
2020-12-07 14:26:46 +0530

    Updates

---
+++ precache-manifest.0a6344f460c68700f4abe0014fb08f63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3fcb896337648c3afbced285938164",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "4dc24bfd01ca967ed3c9",
+    "url": "/calculator/static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "b011ce21a6da794d9886",
+    "url": "/calculator/static/js/2.add7bac3.chunk.js"
+  },
+  {
+    "revision": "4dc24bfd01ca967ed3c9",
+    "url": "/calculator/static/js/main.20114d32.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
